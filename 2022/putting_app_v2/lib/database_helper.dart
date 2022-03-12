241c541265ab12702b8999f62182a2a5393a236f
Janne Ovaska <janneo011@gmail.com>
2022-03-12 10:46:48 -0800

    format the files to remove extra and unneeded code

---
+++ 2022/putting_app_v2/lib/database_helper.dart
@@ -0,0 +1,100 @@
+// I want to make this demo create a table that has all the features that I want in my app.
+// Once all of the features are present, I will work on integrating it into my actual app.
+// My table needs to have
+// [DONE] An ID column
+// A date column
+// A shot type column
+// A Distance column
+// A Throws column
+// A Makes column
+// A putter stack size column
+// This video shows how to access the databases on the emulator https://www.youtube.com/watch?v=GZfFRv9VWtU&t=396s
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DataBaseHelper {
+  static final _dbName = "physicalDeviceDatabase.db";
+  static final _dbVersion = 1;
+  static final _tableName = "myTable";
+
+  static final columnId = "_id";
+  static final columnName = "name";
+  // If everything goes to crap erase the code below here
+  static final columnDate = "date";
+  static final columnShotType = "shotType";
+  static final columnDistance = "distance";
+  static final columnThrows = 'throws';
+  static final columnMakes = 'makes';
+  static final columnStackSize = 'stackSize';
+  // If everything goes to crap erase the code above here
+
+  // Making it a singleton class
+  DataBaseHelper._privateConstructor();
+  static final DataBaseHelper instance = DataBaseHelper._privateConstructor();
+
+  static Database? _database;
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+
+    _database = await _initiateDatabase();
+    return _database;
+  }
+
+  _initiateDatabase() async {
+    Directory? directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, _dbName);
+    print(directory.path);
+    print(_dbName);
+    return await openDatabase(path, version: _dbVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) => db.execute('''
+    CREATE TABLE $_tableName (
+    $columnId INTEGER PRIMARY KEY,
+    $columnName TEXT,
+    $columnDate TEXT,
+    $columnShotType TEXT,
+    $columnDistance INTEGER,
+    $columnThrows INTEGER,
+    $columnMakes INTEGER,
+    $columnStackSize INTEGER)
+    ''');
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database? db = await instance.database;
+    return await db!.insert(_tableName, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAll() async {
+    Database? db = await instance.database;
+    return db!.query(_tableName);
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    Database? db = await instance.database;
+    int id = row[columnId];
+    return await db!.update(
+      _tableName,
+      row,
+      where: "$columnId = ?",
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    Database? db = await instance.database;
+    return await db!
+        .delete(_tableName, where: "$columnId = ?", whereArgs: [id]);
+  }
+}
+
+// structure of the table returned by SQFLITE!
+// {
+//   "_id" : 12,
+// "name" : "name"
+// }
--- 2022/putting_app_v2/lib/main.dart
+++ 2022/putting_app_v2/lib/main.dart
@@ -1,115 +1,519 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'global.dart' as global;
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'database_helper.dart';
 
-void main() {
+// the rgb for the green I want (152,190,100)
+
+void main() async {
   runApp(const MyApp());
+}
+
+//
+// This is the code for writing to a txt file
+//
+// create the class that handles reading and writing to a local text file.
+class DataStorage {
+  // create a function that returns the path to where the file will be stored.
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    // return the path
+    return directory.path;
+  }
+
+  // create a funtion that returns the path to the exact file.
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/puttingData.txt');
+  }
+
+  // create a function that reads the data. If the data cannot be read, return 0.
+  Future<int> readData() async {
+    // try to open and return the contents of the file
+    try {
+      final file = await _localFile;
+
+      final contents = await file.readAsString();
+
+      return int.parse(contents);
+      // if the file cannot be read or parsed, return 0
+    } catch (e) {
+      return 0;
+    }
+  }
+
+  // create a function that writes to the file
+  // when calling the function, the user passes the integer they want to write.
+  Future<File> writeData(int data) async {
+    // wait for the file to open
+    final file = await _localFile;
+    // writes the passed in data as a string to the text document.
+    return file.writeAsString('$data');
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  static const String _title = 'Putting App';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: _title,
+        home: MyStatelessWidget(storage: DataStorage()),
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.green,
+        ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+//
+// This is where I set the main structure of the app.
+// Add features to the app by putting them in the scaffold
+//
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key, required this.storage}) : super(key: key);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final DataStorage storage;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return DefaultTabController(
+      initialIndex: global.startTab,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 0,
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Text('Setup'),
+              ),
+              Tab(
+                icon: Text('Putt'),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          physics: NeverScrollableScrollPhysics(),
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Center(
+              // This calls the Putting Setup page code
+              child: PuttingSetup(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Center(
+              // This is where the putting game code class will be called from
+              child: PuttingCounter(),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class PuttingSetup extends StatelessWidget {
+  const PuttingSetup({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _ChoiceChip(),
+        const Divider(),
+        const StackSizeSliders(),
+        const Center(
+            child: Text(
+          "# of Putters",
+          textScaleFactor: 1.25,
+        )),
+        const Divider(),
+        const DistanceSliders(),
+        const Center(
+            child: Text(
+          "Distance To Basket (ft)",
+          textScaleFactor: 1.25,
+        ))
+      ],
+    );
+  }
+}
+
+//
+// This is where the code for the Choice Chips goes
+//
+
+class _ChoiceChip extends StatefulWidget {
+  @override
+  _ChoiceChipState createState() => _ChoiceChipState();
+}
+
+class _ChoiceChipState extends State<_ChoiceChip> with RestorationMixin {
+  final RestorableIntN _indexSelected = RestorableIntN(global.shotType);
+
+  @override
+  String get restorationId => 'choice_chip_demo';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_indexSelected, 'choice_chip');
+  }
+
+  @override
+  void dispose() {
+    _indexSelected.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Wrap(
+        children: [
+          ChoiceChip(
+            label: const Text("Hyzer"),
+            selected: _indexSelected.value == 0,
+            onSelected: (value) {
+              setState(() {
+                _indexSelected.value = value ? 0 : -1;
+                global.shotType = 0;
+              });
+            },
+          ),
+          const SizedBox(width: 8),
+          ChoiceChip(
+            label: const Text("Flat"),
+            selected: _indexSelected.value == 1,
+            onSelected: (value) {
+              setState(() {
+                _indexSelected.value = value ? 1 : -1;
+                global.shotType = 1;
+              });
+            },
+          ),
+          const SizedBox(width: 8),
+          ChoiceChip(
+            label: const Text("Anhyzer"),
+            selected: _indexSelected.value == 2,
+            onSelected: (value) {
+              setState(() {
+                _indexSelected.value = value ? 2 : -1;
+                global.shotType = 2;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//
+// This is where the stackSize Slider Code goes
+//
+
+class StackSizeSliders extends StatefulWidget {
+  const StackSizeSliders({Key? key}) : super(key: key);
+
+  @override
+  StackSizeSlidersState createState() => StackSizeSlidersState();
+}
+
+class StackSizeSlidersState extends State<StackSizeSliders>
+    with RestorationMixin {
+  final RestorableDouble _continuousValue =
+      RestorableDouble(global.stackSize.toDouble());
+
+  @override
+  String get restorationId => 'stackSize_slider';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_continuousValue, 'continuous_value');
+  }
+
+  @override
+  void dispose() {
+    _continuousValue.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Semantics(
+                child: SizedBox(
+                  width: 64,
+                  height: 48,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    onSubmitted: (value) {
+                      final newValue = double.tryParse(value);
+                      if (newValue != null &&
+                          newValue != _continuousValue.value) {
+                        setState(() {
+                          _continuousValue.value = newValue.clamp(0, 20);
+                          global.stackSize = newValue.clamp(0, 20).toInt();
+                        });
+                      }
+                    },
+                    keyboardType: TextInputType.number,
+                    controller: TextEditingController(
+                      text: _continuousValue.value.toStringAsFixed(0),
+                    ),
+                  ),
+                ),
+              ),
+              Slider(
+                value: _continuousValue.value,
+                min: 0,
+                max: 20,
+                onChanged: (value) {
+                  setState(() {
+                    _continuousValue.value = value;
+                    global.stackSize = value.toDouble().toInt();
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//
+// This is where the distance slider code goes
+
+class DistanceSliders extends StatefulWidget {
+  const DistanceSliders({Key? key}) : super(key: key);
+
+  @override
+  DistanceSlidersState createState() => DistanceSlidersState();
+}
+
+class DistanceSlidersState extends State<DistanceSliders>
+    with RestorationMixin {
+  final RestorableDouble _continuousValue =
+      RestorableDouble(global.distance.toDouble());
+
+  @override
+  String get restorationId => 'distance_slider';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_continuousValue, 'continuous_value');
+  }
+
+  @override
+  void dispose() {
+    _continuousValue.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Semantics(
+                child: SizedBox(
+                  width: 64,
+                  height: 48,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    onSubmitted: (value) {
+                      final newValue = double.tryParse(value);
+                      if (newValue != null &&
+                          newValue != _continuousValue.value) {
+                        setState(() {
+                          _continuousValue.value =
+                              newValue.clamp(0, 100) as double;
+                          global.distance = newValue.clamp(0, 100).toInt();
+                        });
+                      }
+                    },
+                    keyboardType: TextInputType.number,
+                    controller: TextEditingController(
+                      text: _continuousValue.value.toStringAsFixed(0),
+                    ),
+                  ),
+                ),
+              ),
+              Slider(
+                value: _continuousValue.value,
+                min: 0,
+                max: 100,
+                onChanged: (value) {
+                  setState(() {
+                    _continuousValue.value = value;
+                    global.distance = value.toDouble().toInt();
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//
+// This is where the putting counter code goes
+//
+
+class PuttingCounter extends StatelessWidget {
+  const PuttingCounter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Counter();
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({Key? key}) : super(key: key);
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  var count = global.count;
+  var stackSize = global.stackSize;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        return Row(children: [
+          GestureDetector(
+              onTap: () => setState(() {
+                    if (count - stackSize <= 0) {
+                      count = 0;
+                    } else {
+                      count = count - stackSize;
+                    }
+                    global.count = count;
+                    if (count >= global.goal) {
+                      global.backgroundColor = global.green;
+                    } else {
+                      global.backgroundColor = Colors.transparent;
+                    }
+                  }),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                      height: constraints.maxHeight,
+                      width: (MediaQuery.of(context).size.width / 2),
+                      color: global.backgroundColor))),
+          GestureDetector(
+              onTap: () => setState(() {
+                    count = count + stackSize;
+                    global.count = count;
+                    if (count >= global.goal) {
+                      global.backgroundColor = global.green;
+                    } else {
+                      global.backgroundColor = Colors.transparent;
+                    }
+                  }),
+              child: Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                      height: constraints.maxHeight,
+                      width: (MediaQuery.of(context).size.width / 2),
+                      color: global.backgroundColor))),
+        ]);
+      }),
+      Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Center(
+            child: IgnorePointer(
+                child: Text(
+          count.toString(),
+          textScaleFactor: 10,
+        ))),
+        ElevatedButton(
+            child: const Text(
+              "Log Session",
+              textScaleFactor: 2,
+            ),
+            style: null,
+            onPressed: () async {
+              int? i = await DataBaseHelper.instance.insert({
+                DataBaseHelper.columnName: global.name,
+                DataBaseHelper.columnDate: DateTime.now().toString(),
+                DataBaseHelper.columnThrows: global.count,
+                DataBaseHelper.columnMakes: global.makes,
+                DataBaseHelper.columnShotType: global.shotType,
+                DataBaseHelper.columnDistance: global.distance,
+                DataBaseHelper.columnStackSize: global.stackSize
+              });
+              print('the inserted id is $i');
+            }),
+        // ElevatedButton(
+        //     child: const Text(
+        //       "Query Database",
+        //       textScaleFactor: 2,
+        //     ),
+        //     style: null,
+        //     onPressed: () async {
+        //       List<Map<String, dynamic>> queryRows =
+        //           await DataBaseHelper.instance.queryAll();
+        //       print(queryRows);
+        //     }),
+        // ElevatedButton(
+        //     child: const Text(
+        //       "ListView Builder",
+        //       textScaleFactor: 2,
+        //     ),
+        //     style: null,
+        //     onPressed: () {
+        //       ListViewButtons();
+        //       print('The listview should be showing now');
+        //     })
+      ])
+    ]);
+  }
+}
+
+class ListViewButtons extends StatelessWidget {
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+
+  ListViewButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[colorCodes[index]],
+                child: Center(child: Text('Entry ${entries[index]}')),
+              );
+            }));
   }
 }
