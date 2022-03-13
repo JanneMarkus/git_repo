ad6948880f3b93a525cda06e62bdfd7f400ceec6
Janne Ovaska <janneo011@gmail.com>
2022-03-13 11:53:42 -0700

    finish flutter UI design colab

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(
     FriendlyChatApp(),
   );
 }
+
+final ThemeData kIOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = ThemeData(
+  colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+      .copyWith(secondary: Colors.orangeAccent[400]),
+);
+
+String _name = 'Janne';
 
 class FriendlyChatApp extends StatelessWidget {
   const FriendlyChatApp({
@@ -15,7 +30,50 @@ class FriendlyChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FriendlyChat',
-      home: ChatScreen(),
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: const ChatScreen(),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage(
+      {required this.text, required this.animationController, Key? key})
+      : super(key: key);
+  final String text;
+  final AnimationController animationController;
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      axisAlignment: 0.0,
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(_name, style: Theme.of(context).textTheme.headline4),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -27,35 +85,98 @@ class ChatScreen extends StatefulWidget {
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = [];
   final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+  bool _isComposing = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('FriendlyChat')),
-      body: _buildTextComposer(),
+      appBar: AppBar(
+        title: Text('FriendlyChat'),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
+            ),
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            ),
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS // NEW
+            ? BoxDecoration(
+                // NEW
+                border: Border(
+                  // NEW
+                  top: BorderSide(color: Colors.grey[200]!), // NEW
+                ), // NEW
+              ) // NEW
+            : null, // NEW
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    for (var message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
   }
 
   Widget _buildTextComposer() {
     return IconTheme(
       data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
       child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          margin: const EdgeInsets.symmetric(horizontal: 4.0),
           child: Row(
             children: [
               Flexible(
                 child: TextField(
                   controller: _textController,
-                  onSubmitted: _handleSubmitted,
-                  decoration: const InputDecoration.collapsed(
-                      hintText: 'Send a message'),
+                  onChanged: (text) {
+                    setState(() {
+                      _isComposing = text.isNotEmpty;
+                    });
+                  },
+                  onSubmitted: _isComposing ? _handleSubmitted : null,
+                  decoration:
+                      InputDecoration.collapsed(hintText: 'Send a message'),
+                  focusNode: _focusNode,
                 ),
               ),
-              IconButton(
-                onPressed: () => _handleSubmitted(_textController.text),
-                icon: const Icon(Icons.send),
-              )
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Theme.of(context).platform == TargetPlatform.iOS
+                      ? // MODIFIED
+                      CupertinoButton(
+                          // NEW
+                          child: const Text('Send'), // NEW
+                          onPressed: _isComposing // NEW
+                              ? () =>
+                                  _handleSubmitted(_textController.text) // NEW
+                              : null,
+                        )
+                      : // NEW
+                      IconButton(
+                          // MODIFIED
+                          icon: const Icon(Icons.send),
+                          onPressed: _isComposing
+                              ? () => _handleSubmitted(_textController.text)
+                              : null,
+                        )),
             ],
           )),
     );
@@ -63,8 +184,20 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    var message = ChatMessage(
+      text: text,
+      animationController: AnimationController(
+        duration: const Duration(milliseconds: 200),
+        vsync: this,
+      ),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _focusNode.requestFocus();
+    message.animationController.forward();
   }
 }
-
-
-// LEFT OFF HERE https://codelabs.developers.google.com/codelabs/flutter#5
